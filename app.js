3e2f29650a636e4a54561afd0264b9c3e226b2da
Henry Troutman <henguin1001@gmail.com>
2014-08-12 14:25:27 -0700

    changed shortId characters, renamed somethings

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 app.http().io();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -20,13 +20,14 @@ app.use(express.session({
 	secret: 'henry is the greatest'
 }));
 
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,9 @@
 ////////////
 
 var shortId = require('shortid');
+shortId.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
 module.exports = function(app){
-	app.io.route('ready', function(req){
-		// req.io.join('test');
-		
-	});
 	// when a remote needs a new id
 	app.io.route('server-get_session_id',function(req){
 		var session_id = shortId.generate();
@@ -21,10 +18,10 @@ module.exports = function(app){
 	// when a display needs to connect to an id, and a confirmation
 	app.io.route('server-set_session_id',function(req){
 		var session_id = req.data;
-		req.io.emit('server-confirm_id', session_id);
 		req.io.join(session_id);
 		req.session.session_id = session_id;
 		req.session.save(function(){});
+		req.io.emit('server-confirm_id', session_id);
 	});
 	
 	app.io.route('server-unbind',function(req){
@@ -37,7 +34,7 @@ module.exports = function(app){
 
 	// SOON TO BE set_location
 	app.io.route('pageLoad',function(req){
-		app.io.room(req.session.session_id).broadcast('locationChange', req.data);
+		app.io.room(req.session.session_id).broadcast('server-set_location', req.data);
 	});
 
 	app.get('/',function(req, res){
