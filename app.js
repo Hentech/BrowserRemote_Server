5797746fa1dc7934a6125d33ffc6e9e27f31635e
Henry Troutman <henguin1001@gmail.com>
2014-08-12 17:16:51 -0700

    made it run on only socket.io no more express, fixed 500 error

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
-/**
- * Module dependencies.
- */
+var app = require('http').createServer(function() {
 
-var express = require('express.io');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-app.http().io();
-
-// all environments
-app.set('port', process.env.PORT || 80);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(express.cookieParser());
-app.use(express.session({
-	secret: 'henry is the greatest'
-}));
-
-
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
-}
-
-routes(app);
-
-app.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
 });
+var io = require('socket.io')(app);
+require('./routes')(io);
+app.listen(80);
--- routes/index.js
+++ routes/index.js
@@ -6,42 +6,35 @@
 var shortId = require('shortid');
 shortId.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 
-module.exports = function(app){
-	// when a remote needs a new id
-	app.io.route('server-get_session_id',function(req){
-		var session_id = shortId.generate();
-		req.io.emit('server-incoming_id', session_id);
-		req.io.join(session_id);
-		req.session.session_id = session_id;
-		req.session.save(function(){});
-	});
-	// when a display needs to connect to an id, and a confirmation
-	app.io.route('server-set_session_id',function(req){
-		var session_id = req.data;
-		req.io.join(session_id);
-		req.session.session_id = session_id;
-		req.session.save(function(){});
-		req.io.emit('server-confirm_id', session_id);
-	});
-	
-	app.io.route('server-unbind',function(req){
-		var session_id = req.session.session_id;
-		req.io.leave(session_id);
-		req.session.session_id = undefined;
-		req.session.save(function(){});
-	});
 
+module.exports = function(io){
 
-	// SOON TO BE set_location
-	app.io.route('pageLoad',function(req){
-		app.io.room(req.session.session_id).broadcast('server-set_location', req.data);
+	io.on('connection',function(socket){
+		var session_id;
+		// when a remote needs a new id
+		socket.on('server-get_session_id',function(){
+			session_id = shortId.generate();
+			socket.join(session_id);
+			socket.emit('server-incoming_id', session_id);
+
+		});
+
+		// when a display needs to connect to an id, and a confirmation
+		socket.on('server-set_session_id',function(id){
+			session_id = id;
+			socket.join(session_id);
+			socket.emit('server-confirm_id', session_id);
+		});
+		
+		socket.on('server-unbind',function(){
+			socket.leave(session_id);
+			session_id = undefined;
+		});
+		// SOON TO BE set_location
+		socket.on('pageLoad',function(location){
+			console.log("test:" + session_id);
+			socket.to(session_id).emit('server-set_location', location);
+		});
 	});
-
-	app.get('/',function(req, res){
-		res.sendfile('views/test.html');
-	});
-	
-
-
 };
 
